f79bc4c8ab4d16a5c010cfdf7d6366b64716c46d
Mike Edwards <michael.edwards.4108@gmail.com>
2021-05-17 07:57:37 -0400

    rewrote methods for dinosaur and robot classes to properly function

--- dinosaurs.py
+++ dinosaurs.py
@@ -4,23 +4,49 @@ class Dinosaur:
         self.energy = 65000000
         self.attack_power = attack_power
         self.health = 500
+        self.is_dying = False
+        self.is_dead = False
 
-    def tail_swing_attack(self, robots):
-        print(self.type, f" used Tail Swing to attack a Robot!")
-        self.energy -= 1500000
-        robots.health -= 42
+    def tail_swing_attack(self, robot):
+        if self.energy >= 42250000:
+            print(f"{self.type} ferociously used TAIL SWING on {robot.name}!")
+            self.energy -= 3900000
+            robot.health -= self.attack_power
+            if robot.health < 50:
+                print(f"Looks like {robot.name} is breaking down...SEND HIM TO THE SCRAP YARD!")
+        else:
+            print(f"{self.type} missed!")
 
-    def claw_swipe_attack(self, robots):
-        print(self.type, f" used Claw Swipe to attack a Robot!")
-        self.energy -= 1000000
-        robots.health -= 35
+    def claw_swipe_attack(self, robot):
+        if self.energy >= 9750000:
+            print(f"{self.type} used a frenzied CLAW SWIPE at {robot.name}!")
+            self.energy -= 1300000
+            robot.health -= self.attack_power + 5
+            if robot.health < 50:
+                print(f"Looks like {robot.name} is breaking down...SEND HIM TO THE SCRAP YARD!")
+        else:
+            print(f"{self.type} missed!")
 
-    def charge_attack(self, robots):
-        print(self.type, f" used Charge to attack a robot!")
-        self.energy -= 2000000
-        robots.health -= 55
+    def charge_attack(self, robot):
+        if self.energy >= 45500000:
+            print(f"{self.type} recklessly CHARGED at {robot.name}!")
+            self.energy -= 2600000
+            robot.health -= self.attack_power + 7
+            if robot.health < 50:
+                print(f"Looks like that {robot.name} is breaking down...SEND HIM TO THE SCRAP YARD!")
+        else:
+            print(f"{self.type} missed!")
+
+    def attack(self, robot):
+        attack_type = input('Which attack method do you want obliterate your opponent with? Select <0> or <1> or <2>.')
+        if attack_type == 0:
+            self.tail_swing_attack(robot)
+        elif attack_type == 1:
+            self.claw_swipe_attack(robot)
+        else:
+            self.charge_attack(robot)
 
     def quickly_scurry_away_to_eat_a_smaller_dinosaur(self):
-        print(self.type, f" just ate a young dino *mmmm veal*!")
-        self.energy += 750000
+        print(f"{self.type} just ate a young dino *mmmm veal*!")
+        self.energy += 6500000
         self.health += 50
--- robots.py
+++ robots.py
@@ -1,44 +1,60 @@
+from weapons import Weapons
+
+
 class Robot:
-    def __init__(self, name, weapons):
+    def __init__(self, name,):
         self.name = name
         self.power_level = 700000
         self.health = 750
-        self.weapon = weapons
+        self.weapon_options = [Weapons('Longsword', 60), Weapons('Laser Beam', 35), Weapons('105mm Howitzer', 85)]
+        self.chosen_weapon = None
         self.is_breaking_down = False
         self.is_destroyed = False
 
     def flying_longsword_attack(self, dinosaur):
-        self.weapon = "Longsword"
         if self.power_level >= 455000:
-            print(self.name, f" used FLYING LONGSWORD to attack ", dinosaur.type, f"!")
+            print(f"{self.name} used FLYING LONGSWORD to attack {dinosaur.type}!")
             self.power_level -= 42000
-            dinosaur.health -= 60
+            dinosaur.health -= self.weapon_options[0].attack_power
             if dinosaur.health < 75:
-                print(f"Looks like that ", dinosaur.type, f" is very weak!")
+                print(f"Looks like that {dinosaur.type} is very weak...FINISH HIM!")
         else:
-            print(self.name, f" missed!")
+            print(f"{self.name} missed!")
 
     def long_range_attack(self, dinosaur):
-        self.weapon = "Laser Beam"
         if self.power_level >= 100000:
-            print(self.name, f" used LASER BEAM with pinpoint accuracy on ", dinosaur.type, f"!")
+            print(f"{self.name} used LASER BEAM with pinpoint accuracy on {dinosaur.type}!")
             self.power_level -= 14000
-            dinosaur.health -= 35
+            dinosaur.health -= self.weapon_options[1].attack_power
             if dinosaur.health < 75:
-                print(f"Looks like that ", dinosaur.type, f" is very weak!")
+                print(f"Looks like that {dinosaur.type} is very weak...FINISH HIM!")
         else:
-            print(self.name, f" missed!")
+            print(f"{self.name} missed!")
 
     def hell_in_the_sky_attack(self, dinosaur):
-        self.weapon = "105mm Howitzer"
         if self.power_level >= 500000:
-            print(self.name, f" rained hell from above with a 105mm Howitzer on", dinosaur.type, f"!")
+            print(f"{self.name} RAINED HELL FROM ABOVE with a 105mm Howitzer on {dinosaur.type}!")
             self.power_level -= 75000
-            dinosaur.health -= 85
+            dinosaur.health -= self.weapon_options[2].attack_power
             if dinosaur.health < 75:
-                print(f"Looks like that ", dinosaur.type, f" is very weak!")
+                print(f"Looks like that {dinosaur.type} is very weak...FINISH HIM!")
+        else:
+            print(f"{self.name} missed!")
+
+    def attack(self, dinosaur):
+        weapon_choice = input('Which weapon would you like to send your opponent into extinction? Select <0> or <1> '
+                              'or <2>.')
+        if weapon_choice == 0:
+            self.chosen_weapon = self.weapon_options[0]
+            self.flying_longsword_attack(dinosaur)
+        elif weapon_choice == 1:
+            self.chosen_weapon = self.weapon_options[1]
+            self.long_range_attack(dinosaur)
+        else:
+            self.chosen_weapon = self.weapon_options[2]
+            self.hell_in_the_sky_attack(dinosaur)
 
     def refresh_relube(self):
-        print(self.name, f" just drizzled new oil over their gearboxes!")
+        print(f"{self.name} just drizzled new oil over their gearboxes!")
         self.power_level += 70000
         self.health += 75
